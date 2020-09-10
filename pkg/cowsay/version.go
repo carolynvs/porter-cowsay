56a16b56fb9def86129aa86c195139e32cb12fee
Vaughn Dice <vadice@microsoft.com>
2020-09-10 16:05:54 -0600

    add output flag support for version cmd; update version info
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- pkg/cowsay/version.go
+++ pkg/cowsay/version.go
@@ -1,11 +1,20 @@
 package cowsay
 
 import (
-	"fmt"
-
 	"get.porter.sh/mixin/cowsay/pkg"
+	"get.porter.sh/porter/pkg/mixin"
+	"get.porter.sh/porter/pkg/pkgmgmt"
+	"get.porter.sh/porter/pkg/porter/version"
 )
 
-func (m *Mixin) PrintVersion() {
-	fmt.Fprintf(m.Out, "cowsay mixin %s (%s)\n", pkg.Version, pkg.Commit)
+func (m *Mixin) PrintVersion(opts version.Options) error {
+	metadata := mixin.Metadata{
+		Name: "cowsay",
+		VersionInfo: pkgmgmt.VersionInfo{
+			Version: pkg.Version,
+			Commit:  pkg.Commit,
+			Author:  "Porter Authors",
+		},
+	}
+	return version.PrintVersion(m.Context, opts, metadata)
 }
